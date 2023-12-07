01d012cab78fc716c30b2ce07578aab9b5f7c272
Erik Golinelli <erik@codekraft.it>
2023-12-07 21:18:55 +0100

    adding vite-plugin-pwa
    Took 32 seconds

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,37 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
   build: {
     outDir: './dist'
-  }
+  },
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,mp3,jpg,png,webp,svg}']
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'safari-pinned-tab.svg'],
+      manifest: {
+        name: 'XMAS Game 2023',
+        short_name: 'xmas-game',
+        description: 'The 2023 Christmas game',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
