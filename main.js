4bcb9d3d65ccc50b5bb8aeb8a45b160f84e71dcc
Erik Golinelli <erik@codekraft.it>
2023-12-07 20:22:45 +0100

    Update/apply style responsive style
    Took 33 seconds

--- main.js
+++ main.js
@@ -7,24 +7,23 @@ const app = document.querySelector('#app')
 app.innerHTML = `
 <div class="game-wrapper relative text-white w-screen h-screen flex flex-col justify-center items-center">
     
-      <div id="headline" class="absolute top-10 left-10 flex flex-col gap-0 w-1/3">
-          <h4 class="text-white text-2xl uppercase leading-compact font-regular">Blitz Series</h4>
+      <div id="headline" class="absolute top-10 left-10 flex flex-col gap-0 w-1/2 md:w-1/3 text-center md:text-left">
+          <h4 class="text-white text-xl md:text-2xl uppercase leading-compact font-regular">Blitz Series</h4>
       </div>
       
-      <div class="absolute top-16 left-10 right-10 flex align-start justify-start">
-          <h1 id="title" class="text-7xl uppercase leading-compact font-bold text-red-400 w-1/3" >Portal Lift</h1>      
-          <p id="current-score" class="text-white text-center text-7xl uppercase leading-compact font-medium w-1/3"><span>0</span> Ton</p>
+      <div class="absolute top-16 left-10 right-10 flex md:flex-row flex-col align-start justify-start">
+          <h1 id="title" class="text-3xl md:text-7xl uppercase leading-compact font-bold text-center md:text-left text-red-400 w-full md:w-1/3" >Portal Lift</h1>      
+          <p id="current-score" class="text-white text-center text-3xl md:text-7xl uppercase leading-compact font-medium w-full md:w-1/3"><span></span></p>
       </div>
       
       <div id="scoreboard" class="absolute top-36 flex flex-col gap-0 text-center leading-10">
-          <p id="message" class="text-white text-4xl leading-compact font-light">Click the Pit Lift to <b >Start</b></p>
-          <h3 id="heading"></h3>
+          <p id="message" class="text-white text-xl md:text-4xl leading-compact font-light">Click the Pit Lift to <b >Start</b></p>
       </div>
       
     
     <div id="game-item" class="absolute top-0 left-0 w-screen h-screen">
         <img id="game-background" src="img/portal-01.png.webp" alt="" class="absolute top-0 left-0 w-full h-full object-cover" /> 
-        <img id="game-lift" src="img/portal-02.png.webp" alt="" class="absolute top-0 left-0 w-full h-full object-cover" style="transform: translateY( calc(  var( --game-shift ) * -0.16vh  ) " />  
+        <img id="game-lift" src="img/portal-02.png.webp" alt="" class="absolute top-0 left-0 w-full h-full object-cover" style="transform: translateY( calc(  var( --game-shift ) * -0.12vh  ) " />  
         <img id="game-front" src="img/portal-03.png.webp" alt="" class="absolute top-0 left-0 w-full h-full object-cover" /> 
     </div>
 
@@ -40,11 +39,11 @@ app.innerHTML = `
     <div class="flex align-middle justify-between">
       <div id="action-buttons" class="absolute bottom-10 left-10 flex gap-4 items-center flex-start">
         
-        <button id="sound" class="bg-white bg-opacity-20 hover:bg-opacity-30 border border-white border-1-solid text-white font-medium text-2xl uppercase h-16 px-4">
-            <img src="img/sound.svg" class="h-8">
+        <button id="sound" class="bg-white bg-opacity-20 hover:bg-opacity-30 border border-white border-1-solid text-white font-medium text-base md:text-2xl uppercase h-12 md:h-16 px-4">
+            <img src="img/sound.svg" class="h-6 md:h-8">
         </button>
         
-        <button id="reset" class="bg-white bg-opacity-20 hover:bg-opacity-30 border border-white border-1-solid text-white font-regular text-2xl uppercase h-16 px-4">
+        <button id="reset" class="bg-white bg-opacity-20 hover:bg-opacity-30 border border-white border-1-solid text-white font-medium text-xl md:text-2xl uppercase h-12 md:h-16 px-4">
           Reset
         </button>
         <p class="hidden">
@@ -58,13 +57,13 @@ app.innerHTML = `
       </div>
         
       <div id="logo" class="absolute bottom-10 right-10">
-        <img src="img/logo.png.webp" alt="" class="w-36 float-right">
+        <img src="img/logo.png.webp" alt="" class="w-24 md:w-36 float-right">
       </div>
   </div>
 </div>
 `
 
-new ClickGame({
+const g = new ClickGame({
   app,
   canvas: document.getElementById('game-item'),
   message: document.getElementById('message'),
@@ -75,3 +74,5 @@ new ClickGame({
   scoreSizeSlider: document.getElementById('slider-score-size'),
   resetButton: document.getElementById('reset')
 })
+
+g.canStartNewGame()
